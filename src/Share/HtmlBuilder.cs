3913e5c020cb707a14e268d623ba0349e3f8bef8
niltor <zpty@outlook.com>
2024-10-08 10:42:29 +0800

    文档下拉菜单导航的生成

--- src/Share/HtmlBuilder.cs
+++ src/Share/HtmlBuilder.cs
@@ -338,15 +338,45 @@ public partial class HtmlBuilder
         var navigations = new StringBuilder();
         if (hasBlog)
         {
-            navigations.AppendLine("<a href=\"/blogs.html\" class=\"block py-2 text-lg text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100\">Blogs</a>");
+            navigations.AppendLine(@"<a href=""/blogs.html"" class=""block py-2 text text-lg"">Blogs</a>");
         }
         if (hasDocs)
         {
-            navigations.AppendLine("<a href=\"/docs.html\" class=\"block py-2 text-lg text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100\">Docs</a>");
+            var docLinkHtml = "";
+            foreach (var docName in WebInfo.DocInfos.Select(d => d.Name))
+            {
+                docLinkHtml += $@"<a href=""/docs/{docName}.html"" class=""block px-4 py-2 text"">{docName}</a>" + Environment.NewLine;
+            }
+            var docsMenuHtml = $$"""
+                <div class="relative" x-data="{isOpen: false}">
+                  <div>
+                    <button type="button" @click="isOpen = !isOpen" class="flex items-center gap-x-1 text text-lg">
+                      Docs
+                      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                        data-slot="icon">
+                        <path fill-rule="evenodd"
+                          d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                          clip-rule="evenodd" />
+                      </svg>
+                    </button>
+                  </div>
+                  <div class="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-card" x-show="isOpen" x-cloak
+                    @click.away="isOpen = false" x-transition:enter="transition ease-out duration-100"
+                    x-transition:enter-start="transform opacity-0 scale-95"
+                    x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
+                    x-transition:leave-start="transform opacity-100 scale-100"
+                    x-transition:leave-end="transform opacity-0 scale-95" tabindex="-1">
+                    <div class="py-1" role="none">
+                      {{docLinkHtml}}
+                    </div>
+                  </div>
+                </div>
+                """;
+            navigations.AppendLine(docsMenuHtml);
         }
         if (hasAbout)
         {
-            navigations.AppendLine("<a href=\"/about.html\" target=\"_blank\" class=\"block py-2 text-lg text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100\">About</a>");
+            navigations.AppendLine("<a href=\"/about.html\" target=\"_blank\" class=\"block py-2 text text-lg \">About</a>");
         }
         return navigations.ToString();
     }
--- src/Share/template/index.html.tpl
+++ src/Share/template/index.html.tpl
@@ -6,20 +6,26 @@
     <title>@{Name}</title>
     <meta name="description" content="@{Description}" />
     <base href="/" />
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link rel="stylesheet" href="@{BaseUrl}css/app.css" />
-    <link rel="stylesheet" href="@{BaseUrl}css/markdown.css" />
     <link rel="icon" type="image/png" href="@{BaseUrl}favicon.ico" />
     <script>const baseUrl = '@{BaseUrl}';</script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script src="@{BaseUrl}js/index.js"></script>
+    <style>
+        [x-cloak] {
+          display: none !important;
+        }
+    </style>
 </head>
 
 <body class="dark:bg-neutral-900">
-    <div class="text-white py-2 bg-gray-600 dark:bg-neutral-800">
+    <div class="text-white py-2 bg-block">
     <div class="container mx-auto flex items-center space-x-4">
       <div class="flex-none">
         <a href="/" class="text-2xl font-semibold hidden sm:block">EasyBlog</a>
       </div>
-      <div class="flex-grow text-left flex space-x-4">
+      <div class="flex-grow text-left flex space-x-4 items-center">
          @{navigations}
       </div>
       <div class="flex-none flex items-center">
@@ -32,7 +38,7 @@
     </div>
     </div>
 
-    <div class="dark:bg-neutral-800 py-4 fixed bottom-0 w-full bg-gray-200">
+    <div class="py-4 fixed bottom-0 w-full bg-block">
     <div class="container mx-auto text-center">
         <p class="text-neutral-600 dark:text-neutral-300">
         @{Name}
